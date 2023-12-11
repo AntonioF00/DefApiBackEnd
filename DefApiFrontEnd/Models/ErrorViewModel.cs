583cdf5c299551b20c619966f6146ea6a2469799
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2023-12-11 11:46:19 +0100

    up

---
+++ DefApiFrontEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DefApiFrontEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
